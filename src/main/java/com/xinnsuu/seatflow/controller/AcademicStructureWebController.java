0005d319ab76a3fa8a4566706ceff74ebd1bd05a
Keiaa <confinedrose@duck.com>
2025-10-16 23:51:27 +0800

    Organize import to follow existing code convention.

--- src/main/java/com/xinnsuu/seatflow/controller/AcademicStructureWebController.java
+++ src/main/java/com/xinnsuu/seatflow/controller/AcademicStructureWebController.java
@@ -1,7 +1,5 @@
 package com.xinnsuu.seatflow.controller;
 
-import com.xinnsuu.seatflow.model.AcademicStructure;
-import com.xinnsuu.seatflow.service.AcademicStructureService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xinnsuu.seatflow.model.AcademicStructure;
+import com.xinnsuu.seatflow.service.AcademicStructureService;
 
 @Controller
 @RequestMapping("/sections")
