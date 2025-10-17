9601d894506dc5c0e42f1884f30a698cda8ebdae
Keiaa <confinedrose@duck.com>
2025-10-17 09:11:08 +0800

    Use RestController for API.

--- src/main/java/com/xinnsuu/seatflow/controller/ClassroomLayoutController.java
+++ src/main/java/com/xinnsuu/seatflow/controller/ClassroomLayoutController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.xinnsuu.seatflow.model.ClassroomLayout;
 import com.xinnsuu.seatflow.service.ClassroomLayoutService;
 
-@Controller
+@RestController
 @RequestMapping("/api/layouts")
 public class ClassroomLayoutController {
 	
